599e4a25a8f279ef26159f9dd6d42d2e4f9f6a1d
Lucas Celestino <lucascap899@gmail.com>
2022-06-02 19:38:35 -0300

    adicionando css das tabelas,template e forms

--- src/main/java/com/projeto/p2/Controller/HomeController.java
+++ src/main/java/com/projeto/p2/Controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.projeto.p2.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController
 {
     @GetMapping("/")
-    public String index()
+    public String index(Model model)
     {
-        return "index";
+		return "index";
     }
 }
